52d4090cd7b118dd774ccd61409c88036397ece6
Carl Bishop <carlebishop@gmail.com>
2018-07-27 14:38:51 -0500

    Added solution files
    Added all solution files for Chapter 11

---
+++ Chapter 11/WorkingWithEFCore/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Packt.CS7
+{
+    public class Category
+    {
+        // these propertiesmap to columns in the database
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+
+        // defines a navigation property for related rows
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            // to enable developers to add products to Catergory we must
+            // initialize the navigation property to an empty list
+            this.Products = new List<Product>();
+
+        }
+    }
+}
---
+++ Chapter 11/WorkingWithEFCore/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Packt.CS7
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; }
+        [Column("UnitPrice", TypeName = "money")]
+        public decimal? Cost { get; set; }
+        [Column("UnitsInStock")]
+        public short? Stock { get; set; }
+        public bool Discontinued { get; set; }
+
+        // these two define the foreign key relationship
+        // to the categories table
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
